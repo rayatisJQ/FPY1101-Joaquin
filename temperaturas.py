fceca3d2db8a98c2742361b9848ca631740ec12e
rayatisJQ <Joa.tiznado@duocuc.cl>
2026-06-30 23:04:09 -0400

    Subir ejercicios de programación

---
+++ temperaturas.py
@@ -0,0 +1,19 @@
+temperaturas = []
+suma = 0
+
+
+
+for i in range(5):
+    temperaturas.append = int(input(f"Ingrese la temperatura numero {i+1}: "))
+
+    if temperaturas > -20 and temperaturas < 20:
+        print("Temperatura registrada correctamente")
+    else:
+        print("Temperatura no valida dentro del rango")
+    
+for temperatura in temperaturas:
+    suma += temperatura
+
+promedio = suma / len(temperaturas)
+
+print(promedio)
